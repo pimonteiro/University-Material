9d92d81afb9b6ae3e8f6c4d42c1af2dce6f3c302
Filipe Monteiro <pimonteiro@protonmail.com>
2019-03-12 22:24:35 +0000

    fourth pratical lesson of CG

---
+++ 3year/2semester/CG/P4/cmake-build-debug/CMakeFiles/P4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P4.dir/main.cpp.o"
+  "P4.pdb"
+  "P4"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
